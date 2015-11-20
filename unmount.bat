afaba55fc2804c09993e0c0e053a004de0397e49
Günther Eberl <guenther@eberl.se>
2015-11-20 11:06:12 +0100

    Forgot to also implement the function to parse the ini file here.

--- unmount.bat
+++ unmount.bat
@@ -6,3 +6,7 @@ CALL:getvalue %INIFILE% "drive_letter" "" DRIVELETTER
 
 REM Unmount the drive.
 SUBST %DRIVELETTER%: /d
+
+:getvalue
+FOR /F "eol=; eol=[ tokens=1,2* delims==" %%i in ('FINDSTR /b /l /i %~2= %1') DO SET %~4=%%~j
+GOTO:eof
